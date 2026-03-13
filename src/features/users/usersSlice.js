818351c31764e60115902e33b4f0f82ff294b7d7
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-13 22:01:38 +0430

    remove extra patern from link

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,34 +1,34 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) throw new Error("Failed to fetch users");
-    return await response.json();
-});
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  if (!response.ok) throw new Error("Failed to fetch users")
+  return await response.json()
+})
 
 const usersSlice = createSlice({
-    name: "users",
-    initialState: {
-        users: [],
-        loading: false,
-        error: null,
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchUsers.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(fetchUsers.fulfilled, (state, action) => {
-                state.loading = false;
-                state.users = action.payload;
-            })
-            .addCase(fetchUsers.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.error.message;
-            });
-    },
-});
+  name: "users",
+  initialState: {
+    users: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false
+        state.users = action.payload
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.error.message;
+      })
+  },
+})
 
-export default usersSlice.reducer;
+export default usersSlice.reducer
