3f589b8dfe59bdc63eaa8faa2886c09198530286
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-15 21:19:55 +0430

    add styles to entire page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header"
 import UsersPanel from "./features/users/UserPanel/UsersPanel"
 
 
@@ -5,9 +6,19 @@ function App() {
 
 
   return (
-    <>
-      <UsersPanel />
-    </>
+    <div className="min-h-screen bg-slate-950 text-white">
+      <div className="max-w-5xl mx-auto px-4 py-8">
+        <Header />
+
+        <main className="grid lg:grid-cols-[1.15fr_.85fr] gap-4">
+          <section className="border border-white/10 bg-white/5 rounded-xl">
+            <div className="p-4">
+              <UsersPanel />
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
   )
 }
 
